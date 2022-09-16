5bab5c8b18b2e09443ee816022b511b444fcf6cf
Andrew Marquez <andy2mgcc@gmail.com>
2022-09-15 23:45:00 -0600

    Add collapsible settings; weight input

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,92 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  let barWeight = 45;
+  let otherBarWeight = false;
+
+  // Initialize total weight to bar weight
+  let totalWeight = barWeight;
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Plate Calculator</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+  <strong>Debug</strong>
+  <p>Bar weight is {barWeight}</p>
+  <p>Total weight is {totalWeight}</p>
+  <strong>End Debug</strong>
+  <br />
+  <br />
+  <br />
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <details>
+    <summary> Settings </summary>
+    <fieldset>
+      <legend>Bar Weight</legend>
+      <label>
+        <input
+          type="radio"
+          bind:group={barWeight}
+          name="barWeight"
+          value={45}
+          on:change={() => otherBarWeight = false}
+        />
+        45
+      </label>
+      <label>
+        <input
+          type="radio"
+          bind:group={barWeight}
+          name="barWeight"
+          value={35}
+          on:change={() => otherBarWeight = false}
+        />
+        35
+      </label>
+      <label>
+        <input
+          type="radio"
+          bind:group={barWeight}
+          name="barWeight"
+          value={25}
+          on:change={() => otherBarWeight = false}
+        />
+        25
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="barWeight"
+          bind:value={otherBarWeight}
+        />
+        Other
+        {#if otherBarWeight}
+          <input type="number" name="barWeight" bind:value={barWeight} />
+        {/if}
+      </label>
+    </fieldset>
+  </details>
+  <label for="weight">Weight:</label>
+  <input
+    type="number"
+    id="weight"
+    name="weight"
+    min={barWeight}
+    step={5}
+    bind:value={totalWeight}
+  />
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  details {
+    user-select: none;
+    margin-bottom: 1rem;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  details > summary {
+    cursor: pointer;
+    list-style: none;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  details > summary::-webkit-details-marker /* Safari */ {
+    display: none;
   }
 </style>
